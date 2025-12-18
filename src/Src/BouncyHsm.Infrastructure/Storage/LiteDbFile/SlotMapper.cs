c96ee7fb6104684e4d629531d4b027b9dede0648
harrison314 <harrison314@azet.sk>
2025-12-18 22:18:41 +0100

    Change IsUnplugged to IsPluuged in database.

--- src/Src/BouncyHsm.Infrastructure/Storage/LiteDbFile/SlotMapper.cs
+++ src/Src/BouncyHsm.Infrastructure/Storage/LiteDbFile/SlotMapper.cs
@@ -5,7 +5,7 @@ using Riok.Mapperly.Abstractions;
 
 namespace BouncyHsm.Infrastructure.Storage.LiteDbFile;
 
-[Mapper(EnumMappingStrategy = EnumMappingStrategy.ByName, 
+[Mapper(EnumMappingStrategy = EnumMappingStrategy.ByName,
     EnumMappingIgnoreCase = false,
     ThrowOnMappingNullMismatch = true,
     ThrowOnPropertyMappingNullMismatch = true)]
@@ -17,9 +17,16 @@ internal partial class SlotMapper
     }
 
     [MapperIgnoreTarget(nameof(SlotModel.Created))]
+    [MapProperty(nameof(SlotModel.IsPlugged), nameof(SlotEntity.IsPlugged), Use = nameof(IsPluggedMapper))]
     public partial SlotModel MapSlot(SlotEntity slotEntity);
 
 
     [MapperIgnoreSource(nameof(SlotModel.Created))]
     public partial SlotEntity MapSlot(SlotModel model);
+
+    [UserMapping(Default = false)]
+    private bool IsPluggedMapper(bool? value)
+    {
+        return value ?? false;
+    }
 }
